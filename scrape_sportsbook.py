c6d97b55ad7fa905a0a560b0c528380d5036b239
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-08-29 19:24:43 -0500

    init

---
+++ scrape_sportsbook.py
@@ -0,0 +1,14 @@
+from psaw import PushshiftAPI
+import datetime
+
+api = PushshiftAPI()
+
+start_time = int(datetime.datetime(2022, 8, 28).timestamp())
+
+submissions = list(api.search_submissions(after=start_time,
+                            subreddit='sportsbook',
+                            filter=['url','author', 'title', 'subreddit'],
+                            limit=100))
+
+for submission in submissions:
+    print(submission.title)
